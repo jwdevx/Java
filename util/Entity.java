50e2c15bfb251f4c2c286046bec9acdf4dd943df
Jason Widjaja <z5494973@ad.unsw.edu.au>
2024-08-13 19:58:46 +1000

    added uuid

--- util/Entity.java
+++
@@ -1,5 +0,0 @@
-package Java.util;
-
-public interface Entity {
-
-}
--- util/EntityList.java
+++ util/EntityList.java
@@ -2,12 +2,26 @@ package Java.util;
 // Published on Github - Public
 
 import java.util.List;
+import Java.main.Entity;
 import java.util.ArrayList;
 
 public class EntityList<T extends Entity> {
     private List<T> entities = new ArrayList<>();
 
+    /**
+     * @param id - most probably a String id
+     * @return - the object or null if id is not found
+     */
     public T findEntityById(Object id) {
+        /*
+        for (T entity : entities) {
+            if (entity.getId().equals(id)) {
+                return entity;
+            }
+        }
+        return null;
+         */
+
         return entities.stream()
                        .filter(p -> p.getId().equals(id))
                        .findFirst()
