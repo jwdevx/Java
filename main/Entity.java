787ba43fcc48a61718e3fc2e698d0863169ee524
Jason Widjaja <z5494973@ad.unsw.edu.au>
2024-08-13 20:01:08 +1000

    added uuid

---
+++ main/Entity.java
@@ -0,0 +1,16 @@
+package Java.main;
+
+import java.util.UUID;
+
+public class Entity {
+    private UUID id;
+
+    public Entity(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+}
